0fb46e02fb1517559a710b8f9f1d61f178f156c1
Dan Kingo <danieldejen23@gmail.com>
2025-02-16 11:08:02 +0300

    implement error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,19 +1,25 @@
 import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
+import notFoundImg from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
   return (
-    <div>
+    <div className="flex items-center flex-col min-h-screen justify-center">
       {isRouteErrorResponse(error) ? (
         <>
-          <h1>404</h1>
-          <h3>Page Not Found</h3>
+          <img className="w-96" src={notFoundImg} alt="page not found" />
+          <h1 className="lg:text-4xl sm: text-2xl mt-12">
+            Ohhh! Page Not Found
+          </h1>
+          <p className="mt-4">It seems you have taken a wrong turn.</p>
         </>
       ) : (
         <h1>Unexpected error occured</h1>
       )}
 
-      <Link to="/">go to home</Link>
+      <Link to="/" className="text-[#9781FA] underline">
+        go to home
+      </Link>
     </div>
   );
 };

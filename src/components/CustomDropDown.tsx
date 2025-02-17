dc3db08f2cb70fd10dc655cc613ee35177618617
Dan Kingo <danieldejen23@gmail.com>
2025-02-17 16:37:48 +0300

    create custom dropdown component

---
+++ src/components/CustomDropDown.tsx
@@ -0,0 +1,57 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, User, Settings, HelpCircle, LogOut } from "lucide-react";
+import { Link } from "react-router-dom";
+import CustomButton from "./CustomButton";
+
+const CustomDropDown = () => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <CustomButton>
+            <Avatar className="w-6 h-6">
+              <AvatarImage
+                src="https://plus.unsplash.com/premium_photo-1664533227571-cb18551cac82?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="@shadcn"
+              />
+              <AvatarFallback>DN</AvatarFallback>
+            </Avatar>{" "}
+            {"  "}
+            Dan
+            <ChevronDown size="16px" />
+          </CustomButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <User />
+            <Link to="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings />
+            <Link to="/">Settings & Privacy</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <HelpCircle />
+            <Link to="/">Help & Support</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LogOut />
+            <Link to="/">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default CustomDropDown;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,6 @@
-import { ChevronDown, HelpCircle, LogOut, Settings, User } from "lucide-react";
 import logo from "../assets/images/logo-cropped.svg";
 import ToggleDarkMode from "./ToggleDarkMode";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import CustomButton from "./CustomButton";
-import { Link } from "react-router-dom";
+import CustomDropDown from "./CustomDropDown";
 const Navbar = () => {
   return (
     <div className=" shadow-md dark:shadow-gray-900 py-2 lg:px-14 sm:px-4 border-b-2 fixed top-0 w-full">
@@ -20,42 +9,7 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
           <ToggleDarkMode />
 
-          <DropdownMenu>
-            <DropdownMenuTrigger className="focus:outline-none">
-              <CustomButton>
-                <Avatar className="w-6 h-6">
-                  <AvatarImage
-                    src="https://plus.unsplash.com/premium_photo-1664533227571-cb18551cac82?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>DN</AvatarFallback>
-                </Avatar>{" "}
-                {"  "}
-                Dan
-                <ChevronDown size="16px" />
-              </CustomButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <User />
-                <Link to="/profile">Profile</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings />
-                <Link to="/">Settings & Privacy</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <HelpCircle />
-                <Link to="/">Help & Support</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <LogOut />
-                <Link to="/">Logout</Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <CustomDropDown />
         </div>
       </div>
     </div>
